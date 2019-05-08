13b5ea1863e33b6d300d31d75ed4e64a2bd1ee04
Brenden Ehlers <brenden@theehlers.org>
2019-05-08 13:30:55 -0600

    Add entry box to allow multiple personalities

--- StringFixer.py
+++ StringFixer.py
@@ -7,21 +7,20 @@ def main():
     
     
     m = tk.Tk()
-    f = tk.Frame(m, width=100, height=10)
+    topf = tk.Frame(m, width=100, height=1)
+    botf = tk.Frame(m, width=100, height=10)
     
-    l = tk.Label(f, text='', width=75, height=10)
-    b = tk.Button(f, text='Generate Tweet', width=25, command=lambda: l.config(text=mt.tweet(user_id=25073877))) #changeLabelText(l, mt.tweet(user_id=25073877)))
+    l = tk.Label(botf, text='', width=75, height=10) 
+    w = tk.Entry(topf, text='ID', width=25)
+    w.grid(row=0, column=0)
+    b = tk.Button(topf, text='Generate Tweet', width=25, command=lambda: l.config(text=mt.tweet(user_id=int(w.get()))))
+    b.grid(row=0, column=1) 
     
     
-    f.pack()
-    b.pack()
     l.pack()
+    topf.pack()
+    botf.pack()
     m.mainloop()
-    #tweet = mt.tweet(user_id=25073877)
-    #print(tweet)
-    
-def changeLabelText(label, text):
-    label.config(text=text)
 
     
 if __name__ == '__main__':
