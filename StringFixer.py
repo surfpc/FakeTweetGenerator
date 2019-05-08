4050adaaf148d0b0b56253c3c372aedd7e143094
Brenden Ehlers <brenden@theehlers.org>
2019-05-08 13:56:02 -0600

    Add default value to entry

--- StringFixer.py
+++ StringFixer.py
@@ -13,6 +13,7 @@ def main():
     l = tk.Label(botf, text='', width=75, height=10) 
     w = tk.Entry(topf, text='ID', width=25)
     w.grid(row=0, column=0)
+    w.insert(0, '25073877')
     b = tk.Button(topf, text='Generate Tweet', width=25, command=lambda: l.config(text=mt.tweet(user_id=int(w.get()))))
     b.grid(row=0, column=1) 
     
