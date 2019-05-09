36efbe383c4dee12dfe1e65466092207e1f81e32
Brenden Ehlers <brenden@theehlers.org>
2019-05-08 18:09:58 -0600

    Improved readability and organization

--- StringFixer.py
+++ StringFixer.py
@@ -2,6 +2,7 @@
 
 import MarkovTweet as mt
 import tkinter as tk
+import html as html
 
 def main():
     
@@ -10,11 +11,13 @@ def main():
     topf = tk.Frame(m, width=100, height=1)
     botf = tk.Frame(m, width=100, height=10)
     
+    tweet = mt.MarkovTweet("", "", "", "")
+    
     l = tk.Label(botf, text='', width=75, height=10) 
     w = tk.Entry(topf, text='ID', width=25)
     w.grid(row=0, column=0)
     w.insert(0, '25073877')
-    b = tk.Button(topf, text='Generate Tweet', width=25, command=lambda: l.config(text=mt.tweet(user_id=int(w.get()))))
+    b = tk.Button(topf, text='Generate Tweet', width=25, command=lambda: l.config(text=html.unescape(tweet.tweet(user_id=int(w.get())))))
     b.grid(row=0, column=1) 
     
     
